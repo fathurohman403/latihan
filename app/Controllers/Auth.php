c491d74e700a45c16f1f394c474357a8fdf99fdb
fathurohman403 <fathuonline@gmail.com>
2021-02-15 11:55:02 +0700

    login

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -5,12 +5,21 @@ namespace App\Controllers;
 class Auth extends BaseController
 {
 
-public function login()
-{
-    $data = [
-        'user'=>'Fathu',
-        'pass' => 'admin'
-    ];
-    return view('login',$data);
-}
+    public function login()
+    {
+        $data = [
+            'user' => 'Fathu',
+            'pass' => 'admin'
+        ];
+        return view('login', $data);
+    }
+
+    public function cek_login()
+    {
+        $request = service('request');
+        $user = $request->getpost('uname');
+        $password = $request->getpost('psw');
+        echo $user . '<br>';
+        echo $password;
+    }
 }
